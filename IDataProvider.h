80354a74670ad631cf1024911298d30cd24c3fba
shawn <trudgeli@gmail.com>
2017-11-17 02:03:28 +0800

    Finished converting IDataProvider and IEventProcessor

--- IDataProvider.h
+++ IDataProvider.h
@@ -5,4 +5,13 @@
 #ifndef DISRUPTOR_PP_IDATAPROVIDER_H
 #define DISRUPTOR_PP_IDATAPROVIDER_H
 
+namespace Disruptor {
+
+    template <class T>
+    class IDataProvider {
+    public:
+        virtual T operator[](long sequence) = 0;
+    };
+}
+
 #endif //DISRUPTOR_PP_IDATAPROVIDER_H
--- IEventProcessor.h
+++ IEventProcessor.h
@@ -5,4 +5,36 @@
 #ifndef DISRUPTOR_PP_IEVENTPROCESSOR_H
 #define DISRUPTOR_PP_IEVENTPROCESSOR_H
 
+#include "ISequence.h"
+
+namespace Disruptor {
+
+    /// <summary>
+    /// <see cref="IEventProcessor"/> waitFor events to become available for consumption from the <see cref="RingBuffer{T}"/>
+    /// </summary>
+    class IEventProcessor {
+    public:
+        /// <summary>
+        /// Return a reference to the <see cref="ISequence"/> being used by this <see cref="IEventProcessor"/>
+        /// </summary>
+        virtual ISequence GetSequence() = 0;
+
+        /// <summary>
+        /// Signal that this <see cref="IEventProcessor"/> should stop when it has finished consuming at the next clean break.
+        /// It will call <see cref="ISequenceBarrier.Alert"/> to notify the thread to check status.
+        /// </summary>
+        virtual void Halt() = 0;
+
+        /// <summary>
+        /// Starts this instance
+        /// </summary>
+        virtual void Run() = 0;
+
+        /// <summary>
+        /// Gets if the processor is running
+        /// </summary>
+        virtual bool IsRunning() = 0;
+    };
+}
+
 #endif //DISRUPTOR_PP_IEVENTPROCESSOR_H
